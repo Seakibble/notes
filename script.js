6a9c85e69c35e331aa149e6a4ef631cece7daa9a
Max Leeming <max.leeming@gmail.com>
2022-03-10 14:42:03 -0500

    v2

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const $clear = document.getElementById('clear')
 let noteId = 0
 
 function init() {
+    $notes.innerHTML = ''
     if (localStorage.getItem('noteId') != null) {
         for (let i = 0; i <= localStorage.getItem('noteId'); i++) {
             let note = localStorage.getItem(i)
@@ -13,6 +14,10 @@ function init() {
             noteId = i
         }
     } else localStorage.setItem('noteId', noteId)
+
+    if ($notes.innerHTML == '') {
+        $notes.innerHTML = '<div class="empty">Nothing to see here</div>'
+    }
 }
 
 // Add note
@@ -27,10 +32,18 @@ $form.addEventListener('submit', function (e) {
     }
 
     noteId++
+    const now = new Date()
+    const date = [now.getDay(), now.getMonth(), now.getFullYear()].join('/')
+    const time = [now.getHours(), now.getMinutes()].join(':')
     let note = {
         id: noteId,
         title: $form.elements.title.value,
-        text: $form.elements.text.value
+        text: $form.elements.text.value,
+        timestamp: time + ' - ' + date
+    }
+
+    if (document.querySelector('.empty') != null) {
+        document.querySelector('.empty').remove()
     }
 
     displayNote(note)
@@ -46,7 +59,6 @@ $form.addEventListener('submit', function (e) {
 $clear.addEventListener('click', function (e) {
     if (confirm('Whoa! You sure you want to delete all your notes?')) {
         localStorage.clear()
-        $notes.innerHTML = '<h1>Existing Notes</h1>'
         noteId = 0
         init()
     }
@@ -61,18 +73,56 @@ $notes.addEventListener('click', function (e) {
         localStorage.removeItem(id)
         
         $parent.remove()
+        search()
     }
 })
 
 
 function displayNote(note) {
     $notes.innerHTML += `
-    <div class='note'>
+    <div class='note' id='note-${note.id}'>
         <h2>${note.title}</h2>
         <p>${note.text}</p>
+        <span class='timestamp'>Created ${note.timestamp}</span>
         <span class=id>${note.id}</span>
         <button class='delete'>X</button>
     </div>`
 }
 
 init()
+
+
+const $search = document.getElementById('search')
+const $searchBtn = document.getElementById('searchBtn')
+
+function search() {
+    let query = $search.value.toLowerCase()
+    init()
+
+    if (query == '') return
+
+    for (const entry in localStorage) {
+        if (parseInt(entry)) {
+            let note = JSON.parse(localStorage[entry])
+            const $target = document.getElementById('note-' + note.id)
+            if ($target == null) continue
+            
+            if (!note.title.toLowerCase().includes(query) && !note.text.toLowerCase().includes(query)) {
+                $target.remove()
+            } else {
+                $target.classList.add('highlight')
+            }
+        }
+    }
+
+    if ($notes.children.length == 0) {
+        $notes.innerHTML = '<span class="results">No results found</span>'
+    } else {
+        $notes.innerHTML = `<span class="results">Search results: ${$notes.children.length}</span>` + $notes.innerHTML
+    }
+}
+
+let timer = null
+$search.addEventListener('keydown', function () {
+    timer = setTimeout(search, 500)
+})
